84dab08c4f3523044cd40641a8211d4650f58295
crinaivan <137436139+crinaivan@users.noreply.github.com>
2023-07-16 11:58:31 +0300

    Add files via upload

---
+++ js1/index.js
@@ -0,0 +1,56 @@
+// 1. Scrieți o funcție care creează un obiect pentru a păstra informații despre rețeta ta preferată
+
+
+
+//const recipe = {
+//    title : "pizza",
+//    servings : "3",
+//    //ingredients :["ham", "cheese", "kerchup"]
+//}
+//const ingredients = ["ham", "cheese", "kerchup"]
+//
+//console.log (recipe);
+//console.log (ingredients);
+ 
+//2. Scrieți o funcție care returnează un obiect nou fără proprietățile furnizate
+
+//const  input = {
+//    object : { a: 1, b:2},
+//    property : "b"
+//}
+//
+//delete input.property //sterge elementul
+//
+//input.object = { a: 1 }; //output
+//
+//console.log(input);
+
+
+//3. Creați o șir de obiecte în care fiecare obiect descrie o carte și are proprietăți precum titlu (un șir de caractere - string), autor (un șir de caractere - string) și esteCitită (un boolean care indică dacă ați citit-o).
+
+//const book1 = {
+//    titlu : "Cartea 1",
+//    autor : "Autor1",
+//    esteCitita : true
+//}
+//
+//const book2 = {
+//    titlu : "Cartea 2",
+//    autor : "Autor2",
+//    esteCitita : false
+//}
+//
+//
+//if (book1 >= "Cartea 1") {
+//    console.log("Ai citit deja Hobbitul de J.R.R. Tolken");
+//} else  {
+//    console.log("Trebuie sa citesti Hobbitul de J.R.R. Tolken");
+//}
+//
+//if (book2 === "Cartea2"){
+//    console.log("Ai citit deja Hobbitul de J.R.R. Tolken");
+//}else {
+//    console.log("Trebuie sa citesti Hobbitul de J.R.R. Tolken");
+//}
+
+
